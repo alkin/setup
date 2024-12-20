56ee7d6228f00c5c38f00e41f7d22088394e4526
Ricardo Faust <ricardocfaust@gmail.com>
2024-12-20 20:29:56 -0300

    Remove Visual Studio Code installation script and comment out theme configuration in Alacritty setup
    - Deleted the app-vscode.sh script, which included installation and configuration for Visual Studio Code.
    - Commented out the line in app-alacritty.sh that copies the Tokyo Night theme configuration, indicating a potential change in theme management.
    
    This commit simplifies the installation process by removing the VS Code setup and adjusting the Alacritty configuration.

--- install/desktop/app-alacritty.sh
+++ install/desktop/app-alacritty.sh
@@ -3,6 +3,6 @@ yay -S --needed alacritty --noconfirm
 
 mkdir -p ~/.config/alacritty
 cp ~/.local/share/setup/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
-cp ~/.local/share/setup/themes/tokyo-night/alacritty.toml ~/.config/alacritty/theme.toml
+# cp ~/.local/share/setup/themes/tokyo-night/alacritty.toml ~/.config/alacritty/theme.toml
 cp ~/.local/share/setup/configs/alacritty/fonts/CaskaydiaMono.toml ~/.config/alacritty/font.toml
 cp ~/.local/share/setup/configs/alacritty/font-size.toml ~/.config/alacritty/font-size.toml
