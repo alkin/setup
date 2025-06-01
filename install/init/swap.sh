977385221d17c6138ac9bb31cc5aa5e0c9c6c62a
Ricardo Faust <ricardocfaust@gmail.com>
2025-06-01 20:43:21 -0300

    Update swap.sh to create a larger swap file
    - Increased the swap file size from 4GB to 16GB in swap.sh to enhance system performance and memory management.
    - Enabled the swap file creation commands by removing the comment markers for immediate execution.

--- install/init/swap.sh
+++ install/init/swap.sh
@@ -1,5 +1,5 @@
-# sudo dd if=/dev/zero of=/swapfile bs=1M count=4096 status=progress
-# sudo chmod 600 /swapfile
-# sudo mkswap /swapfile
-# sudo swapon /swapfile
-# echo "/swapfile none swap sw 0 0" | sudo tee -a /etc/fstab
+sudo dd if=/dev/zero of=/swapfile bs=1M count=16000 status=progress
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo "/swapfile none swap sw 0 0" | sudo tee -a /etc/fstab
